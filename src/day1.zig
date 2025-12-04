5b88782d113b23785071925ffce38994f798b1dc
Michael Baklor <mbaklor@gmail.com>
2025-12-05 01:34:10 +0200

    brute force day 1 part 2 because I might be stupid

--- src/day1.zig
+++ src/day1.zig
@@ -18,7 +18,7 @@ pub fn main() !void {
     var file_reader: std.fs.File.Reader = file.readerStreaming(&read_buf);
     const reader = &file_reader.interface;
 
-    const sum = try partOne(reader);
+    const sum = try partTwo(reader);
     std.debug.print("sum: {d}\n", .{sum});
 }
 
@@ -48,5 +48,67 @@ fn partOne(reader: *std.Io.Reader) !u16 {
         std.debug.print("position {d}\n", .{position});
         if (position == 0) sum += 1;
     }
+    return sum;
+}
+
+fn partTwo(reader: *std.Io.Reader) !u16 {
+    var sum: u16 = 0;
+    var position: i16 = 50;
+
+    var i: u16 = 1;
+    std.debug.print("0.\tstart:\tposition 50\tsum 0\n", .{});
+    while (try reader.takeDelimiter('\n')) |line| : (i += 1) {
+        var num = try std.fmt.parseInt(i16, line[1..], 10);
+        // const startingPosition = position;
+        std.debug.print("{d}.\t {d} ", .{ i, position });
+        switch (line[0]) {
+            'L' => {
+                std.debug.print("- {d} = ", .{num});
+                while (num != 0) {
+                    position -= 1;
+                    while (position < 0) {
+                        position += 100;
+                    }
+                    while (position > 99) {
+                        position -= 100;
+                    }
+                    if (position == 0) {
+                        sum += 1;
+                    }
+                    num -= 1;
+                }
+            },
+            'R' => {
+                std.debug.print("+ {d} = ", .{num});
+                while (num != 0) {
+                    position += 1;
+                    while (position < 0) {
+                        position += 100;
+                    }
+                    while (position > 99) {
+                        position -= 100;
+                    }
+                    if (position == 0) {
+                        sum += 1;
+                    }
+                    num -= 1;
+                }
+            },
+            else => {
+                std.debug.print("how did we get here?? {s}\n", .{line});
+                break;
+            },
+        }
+        std.debug.print("{d}\t", .{position});
+        const div = @abs(@divFloor(position, 100));
+        // if (startingPosition == 0 and line[0] == 'L' and div != 0) {
+        //     div -= 1;
+        // }
+        // sum += @intCast(div);
+        // position = @mod(position, 100);
+        // if (position == 0 and div == 0) sum += 1;
+        std.debug.print("new position {d}\tdiv {d}\tsum {d}\n", .{ position, div, sum });
+    }
+
     return sum;
 }
