7c346b71c0454dc28f50522b0eb35bf370e478d6
Michael Baklor <mbaklor@gmail.com>
2025-12-14 23:04:01 +0200

    day 3 part 2

--- src/day3.zig
+++ src/day3.zig
@@ -8,7 +8,7 @@ pub fn main() !void {
     var file_reader: std.fs.File.Reader = file.readerStreaming(&read_buf);
     const reader = &file_reader.interface;
 
-    const sum = try partOne(reader);
+    const sum = try partTwo(reader);
     std.debug.print("sum: {d}\n", .{sum});
 }
 
@@ -22,6 +22,18 @@ test "partOne" {
 
     const sum = try partOne(reader);
     try std.testing.expectEqual(@as(u64, 357), sum);
+}
+
+test "partTwo" {
+    const filename = "src/example_day3.txt";
+    var file = try std.fs.cwd().openFile(filename, .{ .mode = .read_only });
+    defer file.close();
+    var read_buf: [1024]u8 = undefined;
+    var file_reader: std.fs.File.Reader = file.readerStreaming(&read_buf);
+    const reader = &file_reader.interface;
+
+    const sum = try partTwo(reader);
+    try std.testing.expectEqual(@as(u64, 3121910778619), sum);
 }
 
 fn partOne(reader: *std.Io.Reader) !u64 {
@@ -41,6 +53,36 @@ fn partOne(reader: *std.Io.Reader) !u64 {
         const full = max * 10 + sec_max;
         sum = sum + full;
         std.debug.print("{s}: ({d} {d})\n", .{ data, max, sec_max });
+        // std.debug.print("\n", .{});
+    }
+    return sum;
+}
+
+fn partTwo(reader: *std.Io.Reader) !u64 {
+    var sum: u64 = 0;
+    while (try reader.takeDelimiter('\n')) |data| {
+        var max_arr: [12]u8 = .{0} ** 12;
+        for (data, 0..data.len) |char, i| {
+            const digit = char - '0';
+            for (0..max_arr.len) |j| {
+                if (digit > max_arr[j] and i < data.len - max_arr.len + j + 1) {
+                    max_arr[j] = digit;
+                    for (j + 1..max_arr.len) |k| {
+                        max_arr[k] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+        var full: u64 = 0;
+        for (max_arr, 0..max_arr.len) |max, i| {
+            full = full + max;
+            if (i < max_arr.len - 1) {
+                full = full * 10;
+            }
+        }
+        sum = sum + full;
+        std.debug.print("{s}: ({any}) {d}\n", .{ data, max_arr, full });
         // std.debug.print("\n", .{});
     }
     return sum;
