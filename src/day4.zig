cc44a98476b459612cd02b6d6e4235d4ab51f05f
Michael Baklor <mbaklor@gmail.com>
2025-12-15 18:04:13 +0200

    day 4 part 2

--- src/day4.zig
+++ src/day4.zig
@@ -11,7 +11,7 @@ pub fn main() !void {
     var file_reader: std.fs.File.Reader = file.readerStreaming(&read_buf);
     const reader = &file_reader.interface;
 
-    const sum = try partOne(allocator, reader);
+    const sum = try partTwo(allocator, reader);
     std.debug.print("sum: {d}\n", .{sum});
 }
 
@@ -25,6 +25,18 @@ test "partOne" {
 
     const sum = try partOne(std.testing.allocator, reader);
     try std.testing.expectEqual(@as(u64, 13), sum);
+}
+
+test "partTwo" {
+    const filename = "src/example_day4.txt";
+    var file = try std.fs.cwd().openFile(filename, .{ .mode = .read_only });
+    defer file.close();
+    var read_buf: [1024]u8 = undefined;
+    var file_reader: std.fs.File.Reader = file.readerStreaming(&read_buf);
+    const reader = &file_reader.interface;
+
+    const sum = try partTwo(std.testing.allocator, reader);
+    try std.testing.expectEqual(@as(u64, 43), sum);
 }
 
 fn partOne(allocator: std.mem.Allocator, reader: *std.Io.Reader) !u64 {
@@ -85,5 +97,72 @@ fn partOne(allocator: std.mem.Allocator, reader: *std.Io.Reader) !u64 {
         prev_line = curr_line;
     }
     allocator.free(curr_line);
+    return sum;
+}
+
+fn partTwo(allocator: std.mem.Allocator, reader: *std.Io.Reader) !u64 {
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    const aa = arena.allocator();
+    defer arena.deinit();
+    var sum: u64 = 0;
+    var file: std.ArrayList([]u8) = .empty;
+    var line_num: usize = 0;
+    while (try reader.takeDelimiter('\n')) |data| : (line_num = line_num + 1) {
+        const line = try aa.dupe(u8, data);
+        try file.append(aa, line);
+    }
+    std.debug.print("\n\nrows {d} cols {d}\n\n", .{ file.items.len, file.items[0].len });
+    var file_contents = file.items;
+    while (true) {
+        var changed: bool = false;
+        for (file_contents, 0..file_contents.len) |line, num| {
+            for (0..line.len) |i| {
+                var count: u8 = 0;
+                if (line[i] == 'x') line[i] = '.';
+                if (line[i] == '.') continue;
+                const first_col = i == 0;
+                const last_col = i == line.len - 1;
+
+                if (num > 0) {
+                    const prev_line = file_contents[num - 1];
+                    if (!first_col) {
+                        if (prev_line[i - 1] == '@') count = count + 1;
+                    }
+                    if (prev_line[i] == '@') count = count + 1;
+                    if (!last_col) {
+                        if (prev_line[i + 1] == '@') count = count + 1;
+                    }
+                }
+
+                if (!first_col) {
+                    if (line[i - 1] == '@') count = count + 1;
+                }
+
+                if (!last_col) {
+                    if (line[i + 1] == '@') count = count + 1;
+                }
+
+                if (num < file_contents.len - 1) {
+                    const next_line = file_contents[num + 1];
+                    if (!first_col) {
+                        if (next_line[i - 1] == '@') count = count + 1;
+                    }
+                    if (next_line[i] == '@') count = count + 1;
+                    if (!last_col) {
+                        if (next_line[i + 1] == '@') count = count + 1;
+                    }
+                }
+
+                if (count < 4) {
+                    sum = sum + 1;
+                    file_contents[num][i] = 'x';
+                    changed = true;
+                }
+            }
+            std.debug.print("{s}\n", .{line});
+        }
+        std.debug.print("\n", .{});
+        if (!changed) break;
+    }
     return sum;
 }
